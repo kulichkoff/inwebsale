650f31cdaf2c424299edd8f528d1fd71957bc45f
kulichkoff <kulichkov@background.digital>
2022-11-03 12:23:13 +0300

    feature/admin button component

--- apps/frontend/admin/pages/[store]/events.tsx
+++ apps/frontend/admin/pages/[store]/events.tsx
@@ -1,11 +1,19 @@
+import { Button } from '@inwebsale/shared/ui';
 import React, { ReactElement } from 'react';
 import BaseLayout from '../../components/BaseLayout';
 import { NextPageWithLayout } from '../_app';
 
 const Events: NextPageWithLayout = () => {
+  function handleClick() {
+    console.log('clicked');
+  }
   return (
     <>
-      <div className="container">events page works</div>
+      <div className="container">events page works
+        <Button clickHandler={handleClick} icon="download">
+          Сохранить
+        </Button>
+      </div>
     </>
   );
 };
--- libs/shared/ui/src/lib/button/button.tsx
+++ libs/shared/ui/src/lib/button/button.tsx
@@ -1,13 +1,20 @@
 import styles from './button.module.scss';
 
+import Image from 'next/image';
+
 /* eslint-disable-next-line */
-export interface ButtonProps {}
+export interface ButtonProps {
+  icon?: 'download';
+  children: React.ReactNode;
+  clickHandler: () => void;
+}
 
 export function Button(props: ButtonProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Button!</h1>
-    </div>
+    <button type="button" className={styles['btn']} onClick={props.clickHandler}>
+      <span>{props.children}</span>
+      {props.icon && <Image src={`/assets/img/icons/${props.icon}.svg`} alt={props.icon} width={32} height={32} />}
+    </button>
   );
 }
 
