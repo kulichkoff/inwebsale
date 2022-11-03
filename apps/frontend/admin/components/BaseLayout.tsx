a9ea8883f8481d1dac578f007ea97fcb53fad111
kulichkoff <kulichkov@background.digital>
2022-11-03 11:20:30 +0300

    feature/admin: made footer

--- apps/frontend/admin/components/BaseLayout.tsx
+++ apps/frontend/admin/components/BaseLayout.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Footer } from './Footer';
 import { Header } from './Header';
 
 export default function BaseLayout({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <div className="wrapper" style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <main style={{ flex: '1 0 auto' }}>{children}</main>
+        <div style={{ flex: '0 0 auto' }}>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
---
+++ apps/frontend/admin/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>© Trade Broadcast 2022</span>
+    </footer>
+  );
+};
